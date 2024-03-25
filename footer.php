95a8a736d3db4f2175ff8baffe9ae79acfee601b
chrisgithub1234 <elioth.barker@gmail.com>
2024-03-25 11:11:17 +0800

    Initial commit.

---
+++ footer.php
@@ -0,0 +1,5 @@
+<footer>
+    <hr>
+    <p style="text-align: center;">Copyright &copy; 2024</p>
+    <hr>
+</footer>
---
+++ navigation.php
@@ -0,0 +1,4 @@
+<a href="index.php">Home</a> | 
+<a href="about.php">About</a> | 
+<a href="about.php">Contact</a> | 
+<hr>
